b2c2e7a9264204573848992c7db500db30b1cc9e
danilovpage <93431459+danilovpage@users.noreply.github.com>
2022-10-14 01:00:01 +0400

    Update test_matrix.py

--- test_matrix.py
+++ test_matrix.py
@@ -11,7 +11,8 @@ data_2 = np.loadtxt(matrix2)
 f.close()
 
 f = open('D:\\res.txt', 'r')
-res = np.loadtxt(f)
+matrix3 = f.readlines()[2:]
+res = np.loadtxt(matrix3)
 f.close()
 
 res_test = np.dot(data_1, data_2)
